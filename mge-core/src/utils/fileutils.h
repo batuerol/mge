ec56543f6de6f3b33d107d2e3f662e393d14c761
mindfog <mindfog>
2016-05-18 03:25:12 +0300

    Temporary file util until platform layer is implemented.

---
+++ mge-core/src/utils/fileutils.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+namespace mge
+{
+	namespace utils
+	{
+		// TODO(batuhan): Temporary. Until platform layer is implemented.
+		static std::string ReadFile(const std::string& fileName)
+		{
+			std::ifstream is(fileName, std::ios_base::in);
+			if (!is.is_open())
+			{
+				// TODO(batuhan): File not found. Log
+				return nullptr;
+			}
+
+			std::string line;
+			std::string result;
+			while(is.eof())
+			{
+				std::getline(is, line);
+				result.append(line + '\n');
+			}
+
+			is.close();
+
+			return result;
+		}
+	}
+}
